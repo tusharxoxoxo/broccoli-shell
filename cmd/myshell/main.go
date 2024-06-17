c74093b95dad627485d75fe9ebcc4bc8f7d6ed44
Tushar Dahiya <shreyashsam@gmail.com>
2024-06-18 00:40:03 +0530

    create the repo

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"bufio"
-	// Uncomment this block to pass the first stage
-	 "fmt"
-	"os"
+    "bufio"
+    "fmt"
+    "os"
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	 fmt.Fprint(os.Stdout, "$ ")
+    fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+    bufio.NewReader(os.Stdin).ReadString('\n')
+
 }
